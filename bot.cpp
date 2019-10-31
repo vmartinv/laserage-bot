6fd1d7eb3bf8f1c7e22b57586cb069189ed58d84
Martin Villagra <mvillagra0@gmail.com>
2019-10-31 04:38:47 -0300

    code cleanup

--- bot.cpp
+++ bot.cpp
@@ -8,26 +8,27 @@ struct pto{
     pto(int x,int y):x(x),y(y){}
 };
 
-static const COLORREF latita=RGB(231,0,49);
-static const COLORREF bolaroja=RGB(255,247,247);
-static const COLORREF bolaazul=RGB(247,255,255);
-static const COLORREF bolaverde=RGB(156,255,49);
-static const COLORREF bolaverde2=RGB(123,231,0);
-static const COLORREF bolaverde3=RGB(140,255,8);
-static const COLORREF gotaazul=RGB(0,24,74);
-static const COLORREF tirocomun=RGB(255,255,0);
-static const COLORREF rayoverde=RGB(247,247,247);
-static const COLORREF tiroblanco=RGB(255,255,247);
-static inline bool istiro(const COLORREF pixel){
-    return pixel==tirocomun   ||
-           pixel==bolaroja    ||
-           pixel==bolaazul    ||
-           pixel==gotaazul    ||
-           pixel==rayoverde   ||
-           pixel==bolaverde   ||
-           pixel==bolaverde2  ||
-           pixel==tiroblanco  ||
-           pixel==bolaverde3;
+static const int pause_key = VK_F6;
+static const COLORREF power_up=RGB(231,0,49);
+static const COLORREF red_ball=RGB(255,247,247);
+static const COLORREF blue_ball=RGB(247,255,255);
+static const COLORREF green_ball=RGB(156,255,49);
+static const COLORREF green_ball2=RGB(123,231,0);
+static const COLORREF green_ball3=RGB(140,255,8);
+static const COLORREF blue_drop=RGB(0,24,74);
+static const COLORREF normal_bullet=RGB(255,255,0);
+static const COLORREF green_ray=RGB(247,247,247);
+static const COLORREF white_bullet=RGB(255,255,247);
+static inline bool is_shoot(const COLORREF pixel){
+    return pixel==normal_bullet   ||
+           pixel==red_ball    ||
+           pixel==blue_ball    ||
+           pixel==blue_drop    ||
+           pixel==green_ray   ||
+           pixel==green_ball   ||
+           pixel==green_ball2  ||
+           pixel==white_bullet  ||
+           pixel==green_ball3;
 }
 
 #define STARTY 320
@@ -39,42 +40,44 @@ static inline bool istiro(const COLORREF pixel){
 #define TOPPER 25
 #define SIDER 20
 
-static inline BYTE *update(){
+static inline BYTE *get_window_pixels(HWND current_window){
     static HDC hdc=NULL, hdcTemp;
     static BYTE* bitPointer;
-    if(!hdc){
-        hdc=GetDC(HWND_DESKTOP);
-        hdcTemp = CreateCompatibleDC(hdc);
-        BITMAPINFO bitmap;
-        bitmap.bmiHeader.biSize = sizeof(bitmap.bmiHeader);
-        bitmap.bmiHeader.biWidth = ENDX-STARTX;
-        bitmap.bmiHeader.biHeight = STARTY-ENDY;
-        bitmap.bmiHeader.biPlanes = 1;
-        bitmap.bmiHeader.biBitCount = 32;
-        bitmap.bmiHeader.biCompression = BI_RGB;
-        bitmap.bmiHeader.biSizeImage = bitmap.bmiHeader.biWidth * 4 * bitmap.bmiHeader.biHeight;
-        bitmap.bmiHeader.biClrUsed = 0;
-        bitmap.bmiHeader.biClrImportant = 0;
-        HBITMAP hBitmap2 = CreateDIBSection(hdcTemp, &bitmap, DIB_RGB_COLORS, (void**)(&bitPointer), NULL, 0);
-        SelectObject(hdcTemp, hBitmap2);
-    }
+	if (!hdc) {
+		hdc = GetDC(current_window);
+		hdcTemp = CreateCompatibleDC(hdc);
+		BITMAPINFO bitmap;
+		bitmap.bmiHeader.biSize = sizeof(bitmap.bmiHeader);
+		bitmap.bmiHeader.biWidth = ENDX - STARTX;
+		bitmap.bmiHeader.biHeight = STARTY - ENDY;
+		bitmap.bmiHeader.biPlanes = 1;
+		bitmap.bmiHeader.biBitCount = 32;
+		bitmap.bmiHeader.biCompression = BI_RGB;
+		bitmap.bmiHeader.biSizeImage = bitmap.bmiHeader.biWidth * 4 * bitmap.bmiHeader.biHeight;
+		bitmap.bmiHeader.biClrUsed = 0;
+		bitmap.bmiHeader.biClrImportant = 0;
+		HBITMAP hBitmap2 = CreateDIBSection(hdcTemp, &bitmap, DIB_RGB_COLORS, (void**)(&bitPointer), NULL, 0);
+		if (hBitmap2 != NULL) {
+			SelectObject(hdcTemp, hBitmap2);
+		}
+	}
     BitBlt(hdcTemp, 0, 0, ENDX-STARTX, ENDY-STARTY, hdc, STARTX, STARTY, SRCCOPY);
     return bitPointer;
 }
 
-static inline pto findspace(){
+static inline pto find_free_space(HWND game_window){
     static int frow[ENDX][ENDY];
     static int islati[ENDX][ENDY];
     static int islati2[ENDX][ENDY];
-    BYTE *p=update();
+    BYTE *p=get_window_pixels(game_window);
     for(int y=STARTY; y<ENDY; y++){
         int cont=0;
         for(int x=STARTX; x<ENDX; x++){
             p+=4;
             COLORREF pix=RGB(p[2], p[1], p[0]);
-            if(pix==latita) islati[x][y]=x;
+            if(pix==power_up) islati[x][y]=x;
             else islati[x][y]=islati[x-1][y]>max(0, x-WIDTH)? islati[x-1][y] : 0;
-            frow[x][y] = istiro(pix)? cont=0 : ++cont;
+            frow[x][y] = is_shoot(pix)? cont=0 : ++cont;
         }
     }
     int q=0;
@@ -109,26 +112,36 @@ static inline pto findspace(){
 
 #define INTERVAL 2
 int main(){
-    srand(time(NULL));
+    srand((unsigned int)time(NULL));
     while(1){
-        while(!(GetKeyState(VK_F6)>>7&1)) Sleep(10);
-        while(GetKeyState(VK_F6)>>7&1) Sleep(10);
-        for(unsigned iter=0;; iter++){
-            if(iter%INTERVAL==0){
-                if(GetKeyState(VK_F6)>>7&1){
-                    mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
-                    Sleep(10);
-                    mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
-                    while(GetKeyState(VK_F6)>>7&1) Sleep(10);
-                    break;
-                }
-                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-            }
-            else if(iter%INTERVAL==INTERVAL/2)
-                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-            pto p=findspace();
-            if(p.x!=-1) SetCursorPos(p.x, p.y), Sleep(10);
-        }
+        while(!(GetKeyState(pause_key)>>7&1)) Sleep(10);
+        while(GetKeyState(pause_key)>>7&1) Sleep(10);
+		HWND current_window = GetForegroundWindow();
+		RECT window_pos;
+		if (!GetWindowRect(current_window, &window_pos))
+			return 1;
+		for (unsigned iter = 0;; iter++) {
+			int width = window_pos.right - window_pos.left;
+			int height = window_pos.bottom - window_pos.top;
+			if (iter % INTERVAL == 0) {
+				if (GetKeyState(pause_key) >> 7 & 1) {
+					mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+					Sleep(10);
+					mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+					while (GetKeyState(VK_F6) >> 7 & 1) Sleep(10);
+					break;
+				}
+				mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+			}
+			else if (iter % INTERVAL == INTERVAL / 2) {
+				mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+			}
+			pto p = find_free_space(current_window);
+			if (p.x != -1) {
+				SetCursorPos(window_pos.left + p.x, window_pos.top + p.y);
+			}
+			Sleep(10);
+		}
     }
     return 0;
 }
